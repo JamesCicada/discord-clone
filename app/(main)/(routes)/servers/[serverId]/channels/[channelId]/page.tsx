8f2b0cfacca27afbd4ac4e2d233e095049df415b
James-Cicada <abdoox.james@gmail.com>
2023-09-15 19:39:34 +0100

    finally

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,56 +1,3 @@
-// import { ChatHeader } from "@/components/chat/chat-header";
-// import { ChatInput } from "@/components/chat/chat-input";
-
-
-// import { currentProfil } from "@/lib/current-profile";
-
-
-// import { db } from "@/lib/db";
-
-// import { redirectToSignIn } from "@clerk/nextjs";
-// import { redirect } from "next/navigation";
-
-// interface ChannelIdPageProps {
-//     params:{
-//         serverId: string;
-//         channelId: string;
-
-//     }
-// }
-
-
-// const ChannelIdPage = async ({params}:ChannelIdPageProps) => {
-//     const profile = await currentProfil()
-//     if(!profile){
-//         return redirectToSignIn()
-//     }
-
-//     const channel = await db.channel.findUnique({
-//         where:{
-//             id: params.channelId
-//         }
-//     })
-//     const member = await db.member.findFirst({
-//         where:{
-//             serverId: params.serverId,
-//             profileId: profile.id
-//         }
-//     })
-
-//     if(!channel || !member) {
-//         return redirect('/')
-//     }
-
-//     return ( 
-//         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-//             <ChatHeader name={channel.name} serverId={channel.serverId} type='channel' />
-//             <div className="flex-1">Future Messages</div>
-//             <ChatInput />
-//         </div>
-//      );
-// }
- 
-// export default ChannelIdPage;
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { ChannelType } from "@prisma/client";
@@ -58,9 +5,9 @@ import { ChannelType } from "@prisma/client";
 import { currentProfil } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
-// import { ChatMessages } from "@/components/chat/chat-messages";
-// import { MediaRoom } from "@/components/media-room";
+import { MediaRoom } from "@/components/media-room";
 import { db } from "@/lib/db";
+import { ChatMessages } from "@/components/chat/chat-messages";
 
 interface ChannelIdPageProps {
   params: {
@@ -104,7 +51,7 @@ const ChannelIdPage = async ({
       />
       {channel.type === ChannelType.TEXT && (
         <>
-          {/* <ChatMessages
+          <ChatMessages
             member={member}
             name={channel.name}
             chatId={channel.id}
@@ -117,10 +64,7 @@ const ChannelIdPage = async ({
             }}
             paramKey="channelId"
             paramValue={channel.id}
-          /> */}
-          <div className="flex-1">
-            Future Chi9lwa
-          </div>
+          />
           <ChatInput
             name={channel.name}
             type="channel"
@@ -132,7 +76,7 @@ const ChannelIdPage = async ({
           />
         </>
       )}
-      {/* {channel.type === ChannelType.AUDIO && (
+      {channel.type === ChannelType.AUDIO && (
         <MediaRoom
           chatId={channel.id}
           video={false}
@@ -145,7 +89,7 @@ const ChannelIdPage = async ({
           video={true}
           audio={true}
         />
-      )} */}
+      )}
     </div>
    );
 }
